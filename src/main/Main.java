a26cf2a32995768735d5b49c96b87901b13cced8
Alvaro Robles <roblesgg16@gmail.com>
2025-05-05 19:48:09 +0200

    Arreglos menores

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/login.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/Login.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         
