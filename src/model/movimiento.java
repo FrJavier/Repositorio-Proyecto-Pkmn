bad40d9e5fd016fc229a330715745fa673d26e63
DuffGang <gangajt1997@gmail.com>
2025-05-07 17:45:23 +0200

    Variables
    movimientos, objetos y pokemon

--- src/model/movimiento.java
+++ src/model/movimiento.java
@@ -1,5 +1,33 @@
 package model;
 
 public class movimiento {
+	
+	private int id_movimiento;
+	private String nom_movimiento;
+	private int nivel_aprendizaje;
+	private int pp_max;
+	private String tipo;
+	private int potencia;
+	private String estado;
+	private int turnos;
+	private int num;
+	
+	public enum TipoMovimiento {
+	    NORMAL,
+	    FUEGO,
+	    AGUA,
+	    PLANTA,
+	    ELECTRICO,
+	    HIELO,
+	    LUCHA,
+	    VENENO,
+	    TIERRA,
+	    VOLADOR,
+	    PSIQUICO,
+	    BICHO,
+	    ROCA,
+	    FANTASMA,
+	    DRAGON;
+	}
 
 }
--- src/model/pokemon.java
+++ src/model/pokemon.java
@@ -1,5 +1,23 @@
 package model;
 
 public class pokemon {
-
+	
+	private int id_pokemon;
+	private int id_entrenador;
+	private int num_pokedex;
+	private String note; //esto deberia ser mote en vez de note, pero esta puesto tal cual en la base de datos
+	private int vitalidad;
+	private int ataque;
+	private int defensa;
+	private int atk_especial;
+	private int def_especial;
+	private int velocidad;
+	private int nivel;
+	private int fertilidad;
+	private char sexo;
+	private String estado;
+	private int equipo;
+	private int id_objeto;
+	private String nombre;
+	
 }
